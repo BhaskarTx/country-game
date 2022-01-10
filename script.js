415941f19c1f834e9ec60d8f3c655a9be55cdce4
Bhaskar Agrawal <8dbdown@gmail.com>
2022-01-10 21:30:40 +0530

    css formatting

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var serverURL = "https://restcountries.com/v2/all?fields=name,population,area,flag";
 var leftName = document.querySelector("#leftTitle");
 var leftPop = document.querySelector("#Lpopulation");
+var leftImg = document.querySelector("#Limg");
+
 var rightName = document.querySelector("#rightTitle");
 var rightPop = document.querySelector("#Rpopulation");
+var rightImg = document.querySelector("#Rimg");
+
 var topText = document.querySelector(".top");
 var scoreText = document.querySelector(".scoreVal");
 var roundText = document.querySelector(".roundNumb");
@@ -21,6 +25,11 @@ var higherBtn = document.querySelector("#higher")
 var lowerBtn = document.querySelector("#lower")
 var nextBtn = document.querySelector("#next")
 
+function showPlayBtn(){
+    play.classList.remove("hide");
+}
+setTimeout(showPlayBtn, 1000);
+
 var arrayOfCountries;
 
 function randomSelect() {
@@ -38,6 +47,8 @@ function loadd() {
     // nextBtn.classList.add("hide")
     play.classList.add("hide");
     rightPop.classList.add("hide");
+    leftImg.classList.remove("hide")
+    rightImg.classList.remove("hide")
     higherBtn.classList.remove("hide")
     lowerBtn.classList.remove("hide")
     topText.classList.remove("hide")
@@ -45,12 +56,14 @@ function loadd() {
     rndm2 = randomSelect();
     leftName.textContent = arrayOfCountries[rndm].name;
     rightName.textContent = arrayOfCountries[rndm2].name;
+    leftImg.src = arrayOfCountries[rndm].flag;
     // console.log(rndm)
     popu1 = Math.ceil(arrayOfCountries[rndm].population / 100) * 100;
     leftPop.textContent = popu1.toLocaleString('en-US');
     popu2 = Math.ceil(arrayOfCountries[rndm2].population / 100) * 100;
     rightPop.textContent = popu2.toLocaleString('en-US');
-
+    rightImg.src = arrayOfCountries[rndm2].flag;
+    
 }
 function scoreColor(){
     if(score>0){
@@ -88,6 +101,7 @@ function highh() {
     rightPop.classList.remove("hide")
     higherBtn.classList.add("hide")
     lowerBtn.classList.add("hide")
+    // leftImg.src = arrayOfCountries[rndm].flag;
     scoreColor();
     
 }
@@ -113,6 +127,7 @@ function lowerr() {
     higher.classList.add("hide")
     lowerBtn.classList.add("hide")
     scoreColor();
+    // leftImg.src = arrayOfCountries[rndm].flag;
 }
 
 function nextt() {
@@ -125,6 +140,7 @@ function nextt() {
     rightPop.classList.add("hide")
     
     rndm = rndm2;
+    leftImg.src = arrayOfCountries[rndm].flag;
     leftName.textContent = arrayOfCountries[rndm].name;
     popu1 = popu2;
     leftPop.textContent = popu1.toLocaleString('en-US');
