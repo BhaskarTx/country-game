0a1f3e5180e7210c7f1975e82d5433e11c60546c
Bhaskar Agrawal <8dbdown@gmail.com>
2022-01-10 19:39:36 +0530

    hide improved

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var topText = document.querySelector(".top");
 var scoreText = document.querySelector(".scoreVal");
 
 var resultVal = document.querySelector("#stat")
+var resultText = document.querySelector("#resultt")
  
 let score = 0;
 scoreText.textContent = score;
@@ -15,27 +16,7 @@ var play = document.querySelector("#play");
 var higherBtn = document.querySelector("#higher")
 var lowerBtn = document.querySelector("#lower")
 var nextBtn = document.querySelector("#next")
-// var serverURL2 = "https://restcountries.com/v3.1/all";
-//  arrayOfCountries = (data) => {
-//     if (!data) return;
 
-//     console.log(data);
-//     //using object destructuring
-//     const { results } = data;
-//     const [profile] = results;
-
-//     const { name } = profile;
-//     displayUserName(name);
-
-//     const { picture } = profile;
-//     displayUserPhoto(picture);
-// }; 
-
-// const arrayOfCountries = ({ flag, name, population, area }) => {
-//     document.querySelector(
-//       "#leftTitle"
-//     ).textContent = `${flag} ${name} ${population} ${area}`;
-//   };
 var arrayOfCountries;
 
 function randomSelect() {
@@ -56,23 +37,21 @@ function loadd() {
     higherBtn.classList.remove("hide")
     lowerBtn.classList.remove("hide")
     topText.classList.remove("hide")
-    // nextBtn.classList.remove("hide")
     rndm = randomSelect();
     rndm2 = randomSelect();
     leftName.textContent = arrayOfCountries[rndm].name;
     rightName.textContent = arrayOfCountries[rndm2].name;
-    // console.log(arrayOfCountries[rndm])
     // console.log(rndm)
-    // console.log(rndm2)
     popu1 = Math.ceil(arrayOfCountries[rndm].population / 100) * 100;
     leftPop.textContent = popu1.toLocaleString('en-US');
     popu2 = Math.ceil(arrayOfCountries[rndm2].population / 100) * 100;
     rightPop.textContent = popu2.toLocaleString('en-US');
-    // console.log(pop1.toLocaleString('en-US'));
 
 }
 
 function highh() {
+    // resultText.textContent = "Your answer is";
+    resultText.classList.remove("hide")
     nextBtn.classList.remove("hide")
     resultVal.classList.remove("hide")
     if (popu2 > popu1) {
@@ -94,6 +73,8 @@ function highh() {
 }
 
 function lowerr() {
+    // resultText.textContent = "Your answer is";
+    resultText.classList.remove("hide")
     nextBtn.classList.remove("hide")
     resultVal.classList.remove("hide")
     if (popu2 < popu1) {
@@ -113,11 +94,14 @@ function lowerr() {
 }
 
 function nextt() {
+    resultText.classList.add("hide");
     resultVal.textContent = "";
+    // resultText.textContent = "";
     higherBtn.classList.remove("hide")
     lowerBtn.classList.remove("hide")
     nextBtn.classList.add("hide")
     rightPop.classList.add("hide")
+    
     rndm = rndm2;
     leftName.textContent = arrayOfCountries[rndm].name;
     popu1 = popu2;
@@ -127,29 +111,13 @@ function nextt() {
     popu2 = Math.ceil(arrayOfCountries[rndm2].population / 100) * 100;
     console.log(rndm2)
     rightPop.textContent = popu2.toLocaleString('en-US');
-    // rightPop.classList.add("hide")
 
 }
 
 play.addEventListener("click", loadd);
-// scoreText.textContent = popu1;
 higherBtn.addEventListener("click", highh);
 lowerBtn.addEventListener("click", lowerr);
 nextBtn.addEventListener("click", nextt)
-// higherBtn.addEventListener("click", function highh() {
-//     score++;
-//     randomSelect();
-//     if (pop1<pop2){
-//         scoreText.textContent = score;
-//         outputBtn.textContent = "correct"
-//     }
-//     else{
-//         outputBtn.textContent = "wrong"
-//     }
-//     console.log(score)
-
-// })
-
 
 
 fetch(serverURL).then(response => response.json())
@@ -159,11 +127,3 @@ fetch(serverURL).then(response => response.json())
         arrayOfCountries = data;
         console.log(arrayOfCountries[0]);
     });
-
-
-// function nextTitles() {
-//     // https://restcountries.com/v2/all?fields=name
-
-// }
-
-// nextBtn.addEventListener("click", nextTitles)
